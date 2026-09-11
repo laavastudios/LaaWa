c8fecec3e34652004d27f59ef34a884d4d0c53ac
Laava Studios <godg59521@gmail.com>
2026-09-12 03:16:00 +0530

    Fix notifications route imports

--- app/api/v1/notifications/route.ts
+++ app/api/v1/notifications/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from "next/server";
-import { apiError, apiSuccess, requireApiAuth } from "../../../../../lib/api";
-import { createNotification, listNotifications, notificationWorkspace, unreadNotificationCount, markNotificationsRead, NOTIFICATION_EVENTS, type NotificationSeverity } from "../../../../../lib/notifications";
-import { query } from "../../../../../lib/db";
+import { apiError, apiSuccess, requireApiAuth } from "../../../../lib/api";
+import { createNotification, listNotifications, notificationWorkspace, unreadNotificationCount, markNotificationsRead, NOTIFICATION_EVENTS, type NotificationSeverity } from "../../../../lib/notifications";
+import { query } from "../../../../lib/db";
 
 export const runtime = "nodejs";
 
