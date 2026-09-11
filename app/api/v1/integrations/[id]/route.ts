3da0cd6eb0f75e505a77d30e3171c09bb9bbe597
Laava Studios <godg59521@gmail.com>
2026-09-12 03:04:53 +0530

    Correct integration detail route import depth

--- app/api/v1/integrations/[id]/route.ts
+++ app/api/v1/integrations/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from "next/server";
-import { apiError, apiSuccess, requireApiAuth } from "../../../../../../lib/api";
-import { decryptIntegrationConfig, getIntegration } from "../../../../../../lib/integrations";
-import { query } from "../../../../../../lib/db";
+import { apiError, apiSuccess, requireApiAuth } from "../../../../../lib/api";
+import { decryptIntegrationConfig, getIntegration } from "../../../../../lib/integrations";
+import { query } from "../../../../../lib/db";
 
 export const runtime = "nodejs";
 
