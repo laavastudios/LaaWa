e5fad90ffcb943c1fc1926b3428bfface291eb51
Laava Studios <godg59521@gmail.com>
2026-09-11 19:33:58 +0530

    feat: add multi-WhatsApp accounts page

---
+++ app/whatsapp/accounts/page.tsx
@@ -0,0 +1,5 @@
+import WhatsAppAccountsHub from "@/components/WhatsAppAccountsHub";
+
+export default function WhatsAppAccountsPage() {
+  return <main className="page-shell"><WhatsAppAccountsHub /></main>;
+}
