5ed9f4e1d471e125072bc994143bd194290e198f
Laava Studios <godg59521@gmail.com>
2026-09-11 23:24:21 +0530

    Phase 22: load Customer 360 base styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./visual-automation.css";
 import "./analytics.css";
 import "./contacts.css";
 import "./intelligence.css";
+import "./customer/customer.css";
 import "./customer/customer-timeline.css";
 
 export const metadata: Metadata = {
