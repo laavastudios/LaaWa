a2fc6f932df1da61f88009095f15adaca7d439d0
Laava Studios <godg59521@gmail.com>
2026-09-11 19:04:36 +0530

    add business page

---
+++ app/business/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import BusinessHub from "@/components/BusinessHub";
+
+export default function BusinessPage() {
+  return <main style={{ minHeight: "100vh", padding: 24, background: "#070a0f" }}><BusinessHub /></main>;
+}
